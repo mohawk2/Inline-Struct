860092d896f165395548ad4fd5f26d2806a76e24
Ed J <mohawk2@users.noreply.github.com>
2014-11-13 03:07:53 +0000

    Release 0.16

--- Struct.pm
+++ Struct.pm
@@ -7,7 +7,7 @@ require Inline::Struct::grammar;
 use Data::Dumper;
 
 use vars qw($VERSION);
-$VERSION = '0.15';
+$VERSION = '0.16';
 
 #=============================================================================
 # Inline::Struct is NOT an ILSM: no register() function

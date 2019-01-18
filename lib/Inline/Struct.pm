981caf0eaeddb6b7fccc60f39bdaf51031001723
Ed J <mohawk2@users.noreply.github.com>
2019-01-18 05:50:41 +0000

    0.24

--- lib/Inline/Struct.pm
+++ lib/Inline/Struct.pm
@@ -7,7 +7,7 @@ require Inline;
 require Inline::Struct::grammar;
 use Data::Dumper;
 
-our $VERSION = '0.23';
+our $VERSION = '0.24';
 
 #=============================================================================
 # Inline::Struct is NOT an ILSM: no register() function
